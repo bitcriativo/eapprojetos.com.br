ff1f2c08b84afffdd656a104ce188b52c5acce81
Eduardo Pacheco <eduardo@eapprojetos.com.br>
2025-05-12 07:48:22 -0300

    chore: configuration webpack

---
+++ webpack.config.js
@@ -0,0 +1,42 @@
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+require('dotenv').config()
+
+module.exports = {
+  mode: process.env.NODE_ENV ?? 'development',
+  devtool: "eval-source-map",
+  entry: ['./src/main.js'],
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          "style-loader",
+          "css-loader",
+          "sass-loader",
+        ],
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      filename: 'index.html',
+      template: './src/index.html'
+    })
+  ],
+  devServer: {
+    static: {
+      directory: path.join(__dirname, 'dist'),
+    },
+    port: 8000,
+  },
+}
